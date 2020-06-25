0d88518a2450330476c9c6176d6b5b6ee243e760
Proful <butu25@gmail.com>
2020-06-25 23:17:31 +1000

    Lesson10: vue instance

--- App.js
+++ App.js
@@ -1,9 +1,13 @@
-var emp = { name: "Steve", age: 23}
+var emp = { name: "Steve", age: 23, changes: []}
 
 var app = new Vue({
     el: "#app",
     data: emp
 })
 
-emp.name = "James"
-app.name = "Rick"
+// emp.name = "James"
+// app.name = "Rick"
+
+app.$watch("name", function(n,old) {
+    console.log(old + " > " + n)
+})
