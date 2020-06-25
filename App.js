e43c51c9986f9aef6b751423c8fcc562dd425a18
Proful <butu25@gmail.com>
2020-06-25 19:51:16 +1000

    reset

--- App.js
+++ App.js
@@ -1,20 +1,5 @@
-Vue.component("Hi", {
-    props: ['name'],
-    template: "<p>Hi {{ name }}</p>",
-});
-
 var app = new Vue({
     el: "#app",
     data: {
-        message: 'Hello Vue',
-        tip: 'something',
-        toggle: false,
-        alphabets: ["a", "b", "c"],
-        name: "Steve"
-    },
-    methods: {
-        onToggle: function () {
-            this.toggle = ! this.toggle
-        }
     }
 });
