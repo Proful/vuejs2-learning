dd16db2bb9a41dae99d2dd3695b686171fbf8085
Proful <butu25@gmail.com>
2020-06-26 19:18:11 +1000

    Lesson13: templates js expression

--- App.js
+++ App.js
@@ -2,6 +2,9 @@ var app = new Vue({
     el: "#app",
     data: {
         name: "Steve",
+        empId: 101,
+        age: 27,
+        isButtonDisabled: true,
         role:'<span style="color: tomato">Developer</span>'
     }
 })
