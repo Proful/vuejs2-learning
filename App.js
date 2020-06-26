345ce6fbb47511ee9b3734aae901a029fba01521
Proful <butu25@gmail.com>
2020-06-26 11:40:45 +1000

    Lesson11: vue instance lifecycle

--- App.js
+++ App.js
@@ -2,12 +2,17 @@ var emp = { name: "Steve", age: 23, changes: []}
 
 var app = new Vue({
     el: "#app",
-    data: emp
-})
-
-// emp.name = "James"
-// app.name = "Rick"
-
-app.$watch("name", function(n,old) {
-    console.log(old + " > " + n)
+    data: emp,
+    created: function () {
+        console.log("created");
+    },
+    mounted: function () {
+        console.log("mounted");
+    },
+    updated: function () {
+        console.log("updated");
+    },
+    destroyed: function () {
+        console.log("destroyed");
+    },
 })
