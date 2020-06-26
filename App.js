a8b74b00ba6a079839f1bc4fbe88a192ccb8c998
Proful <butu25@gmail.com>
2020-06-26 13:44:48 +1000

    Lesson12: templates v-once & v-html

--- App.js
+++ App.js
@@ -1,18 +1,9 @@
-var emp = { name: "Steve", age: 23, changes: []}
-
 var app = new Vue({
     el: "#app",
-    data: emp,
-    created: function () {
-        console.log("created");
-    },
-    mounted: function () {
-        console.log("mounted");
-    },
-    updated: function () {
-        console.log("updated");
-    },
-    destroyed: function () {
-        console.log("destroyed");
-    },
+    data: {
+        name: "Steve",
+        role:'<span style="color: tomato">Developer</span>'
+    }
 })
+
+app.name = "James"
